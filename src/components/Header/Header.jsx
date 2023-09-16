13ebf332c36b08eec057e666986b78da47156f88
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-09-16 17:50:51 +0300

    header/styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
-
 import styles from '../../styles/Header.module.css'
 import { ROUTES } from '../../utils/routes'
-import logo from '../../images/logo.svg'
+import logo from '../../images/logo.png'
 import avatar from '../../images/avatar.jpg'
 
 const Header = () => {
@@ -26,19 +24,32 @@ const Header = () => {
 
         <form lassName={styles.form}>
           <div className={styles.icon}>
-            {/* icon here! */}
+            {/* icon here! search*/}
           </div>
           <div className={styles.input}>
             <input 
-            type="search" 
-            autoComplete='off'
-            name="search"
+             type="search" 
+             autoComplete='off'
+             name="search"
              placeholder='type your text'
              value=""
              onChange={()=>{}}
               />
           </div>
+
+          {/* <div className={styles.box}></div> */}
         </form>
+
+        <div className={styles.account}>
+          <Link className={styles.favourites}to={ROUTES.HOME}>
+            {/* icon here! heart */}
+          </Link>
+          <Link className={styles.cart}to={ROUTES.CART}>
+            {/* icon here! shop cart*/}
+            <span className={styles.count}>2</span>
+          </Link>
+        </div>
+
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
-
+import "./styles/index.css";
 
 import App from './components/App/App.jsx';
 
